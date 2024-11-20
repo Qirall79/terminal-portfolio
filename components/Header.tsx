22f1d1f0d9cc5efe23156c9892b4feb535f1fc40
qirall79 <belfatmi79@gmail.com>
2024-11-20 11:28:10 +0100

    Add more responsiveness for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = ({
@@ -11,8 +12,8 @@ export const Header = ({
   
 
   return (
-    <div className='p-4 flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between items-center mb-12 md:mb-0'>
-      <pre className="text-[10px] md:text-base">
+    <div className='p-4 flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between items-center mb-12 md:mb-0'>
+      <pre className="text-[10px] hidden md:inline-block md:text-base">
         <p>
           {
             '.------------------------------------------------------------------.'
@@ -40,9 +41,11 @@ export const Header = ({
         </p>
       </pre>
 
+      <Image className=" md:hidden" src={"/images/header.png"} width={400} height={300} alt="header" />
+
       <Link
         href={'/' + mode}
-        className='px-4 py-2 border-2 border-slate-300'
+        className='text-xs md:text-base px-4 py-2 border-2 border-slate-300'
       >
         {mode === 'desktop' ? 'Desktop mode' : 'Terminal mode'}
       </Link>
